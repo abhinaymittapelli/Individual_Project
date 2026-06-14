1094654856ab0dabfc29be4cfea26f6b7949fee5
abhinaymittapelli <abhinaymittapally@gmail.com>
2026-06-14 22:42:17 +0530

    Updated API URL for production

--- Rare_Medicine_Locator/Frontend/src/services/api.js
+++ Rare_Medicine_Locator/Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://rare-medicine-locator-1u5n.onrender.com",
+  baseURL: "https://rare-medicine-locator-1u5n.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
